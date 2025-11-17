94c192c6816d66e950d516b6104138b83959debc
aesias <aesias1709@gmail.com>
2025-11-18 08:35:53 +0900

    6025번

--- test.py
+++ test.py
@@ -19,50 +19,62 @@ for i in range(5):
     an += "*"""
 
 """an = input()
-print(an, an, an)"""
+print(an, an, an)
 #6009
 a = input()
 print(a)
+
 #6010
 a = int(input())
 print(a)
+
 #6011
 a = float(input())
 print(a)
+
 #6012
 a= int(input())
 b= int(input())
 print(a)
 print(b)
+
 #6013
 a= input()
 b= input()
 print(b)
 print(a)
+
 #6014
 a= float(input())
 print(a)
 print(a)
 print(a)
+
 #6015
 a, b= input().split()
 print(a)
 print(b)
+
 #6016
 a, b = input().split()
 print(b, a)
+
 #6017
 an = input()
 print(an, an, an)
+
 #6018
 a, b = input().split(":")
 print(a, b, sep=":")
+
 #6019
 a, b, c = input().split(".")
 print(c, b, a, sep="-")
+
 #6020
 a, b = input().split("-")
 print(a, b, sep="")
+
 #6021
 a = input()
 print(a[0])
@@ -70,9 +82,16 @@ print(a[1])
 print(a[2])
 print(a[3])
 print(a[4])
+
 #6022
 a = input()
 print(a[0:2], a[2:4], a[4:6])
+
 #6023
 a = input().split(":")
 print(a[1])
+"""
+#6024
+a, b = input().split()
+c = a+b
+print(c)
