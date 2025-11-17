02890accb74cbf02e7096f9021b0c8b7f64d92eb
aesias <aesias1709@gmail.com>
2025-11-18 08:30:49 +0900

    문제 풀이 09~23

--- test.py
+++ test.py
@@ -20,7 +20,59 @@ for i in range(5):
 
 """an = input()
 print(an, an, an)"""
-
+#6009
+a = input()
+print(a)
+#6010
+a = int(input())
+print(a)
+#6011
+a = float(input())
+print(a)
+#6012
+a= int(input())
+b= int(input())
+print(a)
+print(b)
+#6013
+a= input()
+b= input()
+print(b)
+print(a)
+#6014
+a= float(input())
+print(a)
+print(a)
+print(a)
+#6015
+a, b= input().split()
+print(a)
+print(b)
+#6016
+a, b = input().split()
+print(b, a)
+#6017
+an = input()
+print(an, an, an)
+#6018
+a, b = input().split(":")
+print(a, b, sep=":")
+#6019
+a, b, c = input().split(".")
+print(c, b, a, sep="-")
+#6020
+a, b = input().split("-")
+print(a, b, sep="")
+#6021
+a = input()
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+#6022
+a = input()
+print(a[0:2], a[2:4], a[4:6])
 #6023
 a = input().split(":")
 print(a[1])
