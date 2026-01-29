71df23baafa9d88edb35c90625c4d991bdce9856
aesias <aesias1709@gmail.com>
2026-01-29 14:14:27 +0900

    그룹 나누기

--- test1.py
+++ test1.py
@@ -1,1 +1,17 @@
-print('test1')
+while True:
+    def input_char():
+        char = input("Enter: ").upper()
+        return char
+    if input_char() == "STOP":
+        print("Program stopped.")
+        break
+
+    class gp1:
+        Gp1 = ["A","E","O","Y"]
+        Gp2 = ["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Z"]
+    class gp2:
+        GP1 = []
+    if input_char() in gp1.Gp1:
+        print("Group 1")
+    else:
+        print("Group 2")  
