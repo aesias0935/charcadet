9e30979f7333509859bbd5fa4872d6f1b45209b8
aesias <aesias1709@gmail.com>
2026-01-29 15:43:45 +0900

    문자를 섞고 멈추는 것까지

--- test1.py
+++ test1.py
@@ -1,17 +1,35 @@
-while True:
-    def input_char():
-        char = input("Enter: ").upper()
-        return char
-    if input_char() == "STOP":
-        print("Program stopped.")
-        break
-
-    class gp1:
+import random
+# 문자 그룹 정의
+class gp1:
         Gp1 = ["A","E","O","Y"]
         Gp2 = ["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Z"]
-    class gp2:
+class gp2:
         GP1 = []
-    if input_char() in gp1.Gp1:
+
+# 문자를 섞는 함수
+def shurf():
+    Gp1 = ["A","E","O","Y"]
+    random.shuffle(Gp1)
+    return Gp1
+
+# 문자 입력 함수
+def input_char():
+    char = input("문자를 입력하세요: ").upper()
+    return char
+
+#메인 루프 - 섞을지 멈출지 확인
+while True:
+    char = input_char()
+
+    if char == "STOP":
+        print("Program stopped.")
+        break
+    elif char == "SHUFFLE":
+        print(shurf())
+
+# 문자가 어느 그룹에 속하는지 출력
+    elif char in gp1.Gp1:
         print("Group 1")
     else:
         print("Group 2")  
+#랜덤 값을 가져와서 그 랜덤값을 더함 => 암호화
